e2b2b5fe425e7a98acf958e4255cf4899eb81917
hokton-bd <skmhkt.p@gmail.com>
2020-11-21 16:32:24 +0900

    modify config/app.php

--- app/Http/Controllers/LecturesController.php
+++ app/Http/Controllers/LecturesController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Lectures;
+use App\Models\Teacher;
+use App\Models\Student;
+use App\Models\User;
 
 class LecturesController extends Controller
 {
-    //
+    
+    public function getComingLectures() {
+
+        
+
+    }
+
+
 }
