f8397a561e2f05deed6cf9da083787a9df71bf71
hokton-bd <skmhkt.p@gmail.com>
2020-12-10 19:02:40 +0900

    made teacher_subject_teachable table

---
+++ app/Models/Teacher_subject_teachable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher_subject_teachable extends Model
+{
+    use HasFactory;
+    
+
+
+}
